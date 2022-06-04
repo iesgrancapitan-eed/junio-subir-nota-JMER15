8cff1fe7a824ee833f85825ed86c267b62502d6c
José Miguel <a19esrujo@iesgrancapitan.org>
2022-06-04 15:17:00 +0200

    add project SubidaNotaJunio

---
+++ SubidaNotaJunio/src/mayorOIguales/MayorOIgualesTest.java
@@ -0,0 +1,30 @@
+package mayorOIguales;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class MayorOIgualesTest {
+
+	@Test
+	//Camino 1
+	void testMayorOIguales1() {
+		MayorOIguales prueba1 = new MayorOIguales();
+		assertEquals(prueba1.mayorOIguales(0, 0), "Iguales");
+	}
+	
+	@Test
+	//Camino 2
+	void testMayorOIguales2() {
+		MayorOIguales prueba2 = new MayorOIguales();
+		assertEquals(prueba2.mayorOIguales(7, 2), "a: 7 es mayor");
+	}
+	
+	@Test
+	//Camino 3
+	void testMayorOIguales3() {
+		MayorOIguales prueba3 = new MayorOIguales();
+		assertEquals(prueba3.mayorOIguales(2, 7), "b: 7 es mayor");
+	}
+
+}
